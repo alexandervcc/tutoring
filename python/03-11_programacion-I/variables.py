ed1fec86b6ee9031920db2f951cb96c3e50fc5a3
alexandervcc <alex.charco.25@gmail.com>
2024-03-11 19:13:26 -0500

    Python: clases programacion estructurada

---
+++ python/03-11_programacion-I/variables.py
@@ -0,0 +1,39 @@
+'''
+comentario de varias lineas
+comenterio linea 2
+'''
+#variables
+'''
+-Python es un lenguaje de tipado dinamico
+-No debes darle el tipo a la variable cuando la declaras 
+-Tipos Java: int, double, boolean, String, etc
+-Solo le asignas el valor, y python por detras determina el tipo de dato
+-Una variable puedes actualizar el valor tanto como tipo
+'''
+#int a = 10;
+#variable entero, pero puedes reasignarle un valor de cualquier tipo
+a = 10
+
+#Impresion de valores
+print(a)
+
+#reasigno un double
+a = 10.5
+print(a)
+
+#Strings: van entre comillas dobles
+#reasigno un string
+a = "Hola"
+print(a)
+
+#reasigno un boolean
+#Boolean: empezar con mayuscula
+a = False
+print(a)
+
+sum1 = 5
+sum2 = 6
+print("Suma 1: ",sum1+sum2)
+sum1 = 4
+print("Suma 2: ",sum1+sum2)
+
