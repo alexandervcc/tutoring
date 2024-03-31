362e0ceebbd14294aafd750716e00f5378cd5107
alexandervcc <alex.charco.25@gmail.com>
2024-03-31 15:59:10 -0500

    Query params

--- spring/clases-spring/src/main/java/com/example/demo/controllers/NumerosControlador.java
+++ spring/clases-spring/src/main/java/com/example/demo/controllers/NumerosControlador.java
@@ -2,6 +2,7 @@ package com.example.demo.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,6 +17,21 @@ public class NumerosControlador {
 	@GetMapping(path="/n2")
 	public int num2() {
 		return 200;
+	}
+	
+	@GetMapping(path = "/")
+	public int devolverNumeroRecibido(
+		@RequestParam(name = "search_query", required = false) Integer numero
+	) {
+		System.out.print("\n\n\n\n\n\nNUMERO: "+numero+"\n\n\n\n\n");
+		return numero;
+	}
+	
+	@GetMapping(path = "/potencia_cuadrada")
+	public int calcularPotenciaCuadrada(
+		@RequestParam(name = "n", required = false, defaultValue = "3") Integer numero
+	) {
+		return numero * numero;
 	}
 	
 	
