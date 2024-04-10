350a5729655197f126e1caafeb1611cd52bbb8cb
alexandervcc <alex.charco.25@gmail.com>
2024-04-09 22:33:30 -0500

    Arreglar paquete para DTOs

--- spring/clases-spring/src/main/java/com/example/demo/controllers/LibrosController.java
+++ spring/clases-spring/src/main/java/com/example/demo/controllers/LibrosController.java
@@ -1,6 +1,5 @@
 package com.example.demo.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import dto.LibroDto;
+import com.example.demo.dto.LibroDto;
 
 @RestController
 @RequestMapping(value = "/libros")
--- spring/clases-spring/src/main/java/com/example/demo/dto/LibroDto.java
+++ spring/clases-spring/src/main/java/com/example/demo/dto/LibroDto.java
@@ -1,4 +1,4 @@
-package dto;
+package com.example.demo.dto;
 
 // data transfer object
 public class LibroDto {
