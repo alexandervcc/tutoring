da751c5375c74b072705937df33b750030a15f18
alexandervcc <alex.charco.25@gmail.com>
2024-12-20 18:29:34 -0500

    Cors

--- spring/store/src/main/java/acc/tutoring/store/store/controllers/ProductoController.java
+++ spring/store/src/main/java/acc/tutoring/store/store/controllers/ProductoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 import acc.tutoring.store.store.dto.ProductoReqDto;
 import acc.tutoring.store.store.model.Producto;
 import acc.tutoring.store.store.services.ProductoServicio;
-import jakarta.websocket.server.PathParam;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping(path = "/api/v1/producto")
-
+@CrossOrigin(origins = "*")
 public class ProductoController {
   @Autowired
   private ProductoServicio productoServicio;
