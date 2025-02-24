4316bfdce7a661d09e476d786971ff4c0de36465
alexandervcc <alex.charco.25@gmail.com>
2025-02-24 18:43:59 -0500

    Bucle FOR

--- java/programacion_estructurada/src/main/java/acc/coding/Main.java
+++ java/programacion_estructurada/src/main/java/acc/coding/Main.java
@@ -47,6 +47,7 @@ public class Main {
 
         // Al hacer una comparacion el resultado es un boolean, verdadero o falso
 
+        
         boolean b1 = 70 > 65;
         System.out.println("70 > 65: " + b1);
         boolean b2 = 70 == 65;
@@ -129,5 +130,41 @@ public class Main {
         } else {
             System.out.println("Obligada a votar");
         }
+            
+
+
+
+
+        // -----------------------------------------------------------------------------------------------
+        /*
+            BUCLES
+            - Repeticion de una accion
+            - Tipos de bucles: FOR, WHILE, DO-WHILE
+            - Todo bucle debe de tener una condicion de finalizacion
+        */        
+
+        // FOR
+        // - Bucle que repite la accion senalando un punto de inicio y punto de fin
+        // - Tamano del paso: como queremos que avance desde el punto de inicio hacia el punto de fin
+        // for ( <inicio> ; <condicion de fin> ; <tamano del paso>  ) { 
+        //     ... codigo que se repite
+        // }
+
+        // i ++;   => i = i + 1;
+        System.out.println("Bucle 1");
+        for(int i = 0; i <= 5 ; i++  ){
+            System.out.println("Numero actual: "+i);
+        }
+
+        System.out.println("Bucle 2");
+        for(int i = 10; i <= 30 ; i+=2  ){
+            System.out.println("Numero actual: "+i);
+        }
+        
+        System.out.println("Bucle 3");
+        for(int i = 100; i > -10 ; i-=5  ){
+            System.out.println("Numero actual: "+i);
+        }
+        
     }
 }
