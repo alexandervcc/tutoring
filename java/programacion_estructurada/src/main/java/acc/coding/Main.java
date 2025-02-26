1dcd305749e00ad34fa44be39ed30a898b3b33ca
alexandervcc <alex.charco.25@gmail.com>
2025-02-25 19:48:10 -0500

    Manejo de arreglos

--- java/programacion_estructurada/src/main/java/acc/coding/Main.java
+++ java/programacion_estructurada/src/main/java/acc/coding/Main.java
@@ -1,5 +1,8 @@
 package acc.coding;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
@@ -47,7 +50,7 @@ public class Main {
 
         // Al hacer una comparacion el resultado es un boolean, verdadero o falso
 
-        /*
+        
         boolean b1 = 70 > 65;
         System.out.println("70 > 65: " + b1);
         boolean b2 = 70 == 65;
@@ -165,7 +168,7 @@ public class Main {
         for(int i = 100; i > -10 ; i-=5  ){
             System.out.println("Numero actual: "+i);
         }
-        */
+        
 
 
         //-----------------------------------------------------------------------------------------------
@@ -178,8 +181,8 @@ public class Main {
         // objectos: almacena el valor + metodos, el tipo empieza con mayuscula
         // Por lo tanto, los Strings son objetos
         String n = "mana";
-        int edad = 8;
-        System.out.println(n + " "+edad);
+        int edad1 = 8;
+        System.out.println(n + " "+edad1);
         // Para ejecutar un metodo de un objeto, se utilizar un punto
         System.out.println("Cantidad de letras en `n`: "+n.length());
         // Al usar un . sobre una primitiva no habra funciones/metodos a ejecutar
@@ -192,12 +195,12 @@ public class Main {
         System.out.println( "longitud del string(tamanoOracion): "+ tamanoOracion);
 
         // equals(): comparar si dos strings son iguales
-        String str1 = "mana";
-        String str2 = "mijo";
-        String str3 = "mana";
-        boolean b1 = str1.equals(str2);
-        System.out.println(str1+"=="+str2+ " -> "+b1);
-        System.out.println(str1+"=="+str3+ " -> "+str1.equals(str3));
+        String sstr1 = "mana";
+        String sstr2 = "mijo";
+        String sstr3 = "mana";
+        boolean bol1 = sstr1.equals(sstr2);
+        System.out.println(sstr1+"=="+sstr2+ " -> "+bol1);
+        System.out.println(sstr1+"=="+sstr3+ " -> "+str1.equals(sstr3));
 
         // convertir a mayuscyka o minuscula
         System.out.println("Oracion original: "+oracion);
@@ -265,10 +268,122 @@ public class Main {
         System.out.println("\n\n\n--------------- .charAt()");
         // Obtener el char en una posicion: .charAt()
         String lugar ="Ecuador";
-        char c1 = lugar.charAt(2);
+        char cc1 = lugar.charAt(2);
         System.out.println(lugar);
-        System.out.println("char en posicion 2: "+c1);
+        System.out.println("char en posicion 2: "+cc1);
         
+
+        // --------------------------------------------------------------------------------------------
+        // ARREGLOS
+        System.out.println("\n\n\n\n---------------------------------------------------------------");
+        System.out.println("ARRAYS");
+        // Almacenar multiples valores juntos
+        // Definir la cantidad de elementos juntos
+        // Para acceder los arreglos se utiliza indices
+        // Los arreglos almacenan valores del mismo tipo de dato
+        
+        // Creamos un arreglo con valores iniciales, el tamano/capacidad depende de cuantos valores iniciales tiene
+        int[] numeros1 = {1, 5, 6, 9, 100};  // 5
+        double[] numerosDouble = {1, 5, 6, 9, 100, 25.5};   // 6
+        boolean[] booleanos = {true, false, true};  // 3
+        String[] arregloStrings = {"hola","como","estas   !!!!"};  // 3
+
+        // Arreglo sin valores iniciales, pero con capacidad inicial
+        int[] numeros2 = new int[10];
+
+        System.out.println("numeros1[0]: "+numeros1[0]);
+        System.out.println("numeros1[4]: "+numeros1[4]);
+        System.out.println("numeros2[0]: "+numeros2[0]);
+        System.out.println("numeros2[7]: "+numeros2[7]);
+
+        // Tratar de acceder un indice invalido creara un error
+        // System.out.println("numeros1[10]: "+numeros1[10]);
+
+        // editar elementos del arreglo
+        numeros1[4] = 999999;
+        System.out.println("numeros1[4]: "+numeros1[4]);
+
+
+        // Podemos obtener el tamano de un arreglo
+        int tamanoArregloNumeros1 = numeros1.length;
+        System.out.println("Tamano de numeros1 = "+tamanoArregloNumeros1);
+        
+        // rellenar un arreglo
+        System.out.println("Valores inciales del arreglo numeros 1");
+        System.out.println("numeros1[0] => "+numeros1[0]);
+        System.out.println("numeros1[1] => "+numeros1[1]);
+        System.out.println("numeros1[2] => "+numeros1[2]);
+        System.out.println("numeros1[3] => "+numeros1[3]);
+        System.out.println("numeros1[4] => "+numeros1[4]);
+
+        System.out.println("Rellenando arreglo numeros 1");
+        Arrays.fill(numeros1, 99);
+        
+        System.out.println("numeros1[0] => "+numeros1[0]);
+        System.out.println("numeros1[1] => "+numeros1[1]);
+        System.out.println("numeros1[2] => "+numeros1[2]);
+        System.out.println("numeros1[3] => "+numeros1[3]);
+        System.out.println("numeros1[4] => "+numeros1[4]);
+
+        // Copiar un arreglo
+        System.out.println("\n\n\n Copia de arreglos");
+        String[] copiaArregloStrings = Arrays.copyOf(arregloStrings, arregloStrings.length);
+        System.out.println("copiaArregloStrings[0]: "+copiaArregloStrings[0]);
+        System.out.println("arregloStrings[0]: "+arregloStrings[0]);
+
+        System.out.println("Sobreescribiendo copia");
+        copiaArregloStrings[0] = "adios";
+
+        System.out.println("copiaArregloStrings[0]: "+copiaArregloStrings[0]);
+        System.out.println("arregloStrings[0]: "+arregloStrings[0]);
+
+        System.out.println("\n>>> Recorrer arreglo:");
+        // Recorrido de un arreglo con FOR
+        for(int i = 0; i < numerosDouble.length; i++){
+            System.out.println("numerosDouble["+i+"]: "+numerosDouble[i]);
+        }
+        
+        System.out.println("\nMultiplicando elementos del arreglo por 2 \n");
+        for(int i = 0; i < numerosDouble.length; i++){
+            numerosDouble[i] = numerosDouble[i] * 2;
+        }
+        
+        for(int i = 0; i < numerosDouble.length; i++){
+            System.out.println("numerosDouble["+i+"]: "+numerosDouble[i]);
+        }
+
+        System.out.println("\nRecorrido de arreglo con FOR for(double numero : numerosDouble) ");
+        // FOR especial sin indices
+        for(double numero : numerosDouble){
+            System.out.println("numerosDouble elemento: "+numero);
+        }
+
+        System.out.println("\n\n\nOrdenamiento:");
+        // Ordenamiento: defecto ascendente
+        Arrays.sort(numerosDouble);
+        System.out.println("Ordenamiento ascendente");
+        for(int i = 0; i < numerosDouble.length; i++){
+            System.out.println("numerosDouble["+i+"]: "+numerosDouble[i]);
+        }
+
+        // Ordenamiento descendente: no funciona con primitivas
+        //Arrays.sort(numerosDouble, Collections.reverseOrder());
+        //System.out.println("Ordenamiento descendente");
+        //for(int i = 0; i < numerosDouble.length; i++){
+        //    System.out.println("numerosDouble["+i+"]: "+numerosDouble[i]);
+        //}
+
+
+        System.out.println("\n\n\n\nOrdenar strings");
+
+        for(int i = 0; i < arregloStrings.length; i++){
+            System.out.println("arregloStrings["+i+"]: "+arregloStrings[i]);
+        }
+        Arrays.sort(arregloStrings);
+        for(int i = 0; i < arregloStrings.length; i++){
+            System.out.println("arregloStrings["+i+"]: "+arregloStrings[i]);
+        }
+
 
     }
 }
