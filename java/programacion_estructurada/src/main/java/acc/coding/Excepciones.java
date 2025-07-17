80b974b271fa79b504b1e5bcfbae7446cd0e0fa3
alexandervcc <alex.charco.25@gmail.com>
2025-07-17 17:57:51 -0500

    Excepciones 2

--- java/programacion_estructurada/src/main/java/acc/coding/Excepciones.java
+++ java/programacion_estructurada/src/main/java/acc/coding/Excepciones.java
@@ -2,6 +2,7 @@ package acc.coding;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.Scanner;
 
 public class Excepciones {
     public static void ejecutar() {
@@ -26,7 +27,119 @@ public class Excepciones {
 
         // Excepciones.excepcionNull();
 
-        Excepciones.excepcionChecked();
+        // Excepciones.excepcionChecked();
+
+        // Excepciones.tryCatchEspecializado();
+
+        // Excepciones.lanzarExcepcionManualmente();
+        /*
+         * 
+         * double d = 0;
+         * try {
+         * d = Excepciones.dividirNumerosChecked(5, 0);
+         * System.out.println("Division = " + d);
+         * } catch (Exception e) {
+         * System.out.println("Error en division = " + e);
+         * }
+         * 
+         * d = Excepciones.dividirNumerosUnchecked(5, 0);
+         * System.out.println("Division = " + d);
+         * 
+         * System.out.println(">>> Fin de programa. ");
+         * 
+         */
+
+        Excepciones.tryWithResources();
+    }
+
+    public static void tryWithResources() {
+        // Try-catch especializado para cerrar recursos como archivos, conexiones
+        // Ya no es necesario el cierre manual del recurso.
+        System.out.println(">>> tryWithResources");
+        String ruta = "./dir/archivo.txt";
+
+        try 
+        (
+            BufferedReader buffer = new BufferedReader(new FileReader(ruta))
+        ) 
+        {
+            String contenido = buffer.readLine();
+            System.out.println("Contenido = " + contenido);
+        } catch (Exception e) {
+            System.out.println("Error de lectura = " + e);
+        }
+    }
+
+    public static double dividirNumerosUnchecked(double d1, double d2) {
+        // Esta funcion es de tipo unchecked
+        // Las unchecked utilizar RuntimeException
+        if (d2 == 0) {
+            throw new RuntimeException("No es posible la division para cero.");
+        }
+
+        double d = d1 / d2;
+
+        return d;
+    }
+
+    public static double dividirNumerosChecked(double d1, double d2) throws Exception {
+        // Por defecto se suele tratar a las excepcion de tipo 'Exception' como checked
+        // Esta funcion es de tipo checked
+        if (d2 == 0) {
+            throw new Exception("No es posible la division para cero.");
+        }
+
+        double d = d1 / d2;
+
+        return d;
+    }
+
+    public static void tryCatchEspecializado() {
+        try {
+            // BufferedReader buffer = new BufferedReader(new FileReader("t"));
+            int[] arr = { 1, 2 };
+            arr[5] = 0;
+            int i = 10 / 0;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            // Este bloque catch, solo capturara excepciones del tipo
+            // ArrayIndexOutOfBoundsException
+            System.out.println(">>> Error indice fuera de rango. " + e);
+        } catch (ArithmeticException e) {
+            System.out.println(">>> Error aritmetico. " + e);
+        } catch (Exception e) {
+            System.out.println(">>> Error generico : " + e);
+        }
+        System.out.println(">>> Final de la funcion tryCatchEspecializado");
+    }
+
+    public static void lanzarExcepcionManualmente() {
+        System.out.println("--- Funcion lanzando una excepcion");
+
+        // Utilizar palabra reservada: throw
+        Scanner sc = new Scanner(System.in);
+        try {
+            int num = sc.nextInt();
+            if (num % 2 == 0) {
+                Exception e = new Exception("Numero invalido");
+                throw e; // lanzar una excepcion
+            }
+            System.out.println("Numero aceptado ");
+            // throw new Exception("Error customizado");
+        } catch (Exception e) {
+            System.out.println(">>> Excepcion manual capturada: " + e);
+        }
+    }
+
+    public static void lanzarExcepcionManualmenteVersionReturn() {
+        System.out.println("--- Funcion con return en vez de exception");
+
+        // Utilizar palabra reservada: throw
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        if (num % 2 == 0) {
+            return;
+        }
+        System.out.println("Numero aceptado ");
     }
 
     /**
