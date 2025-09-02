12e06763f356988265f313f4a06cfb72c1932d8e
alexandervcc <alex.charco.25@gmail.com>
2025-09-01 20:30:39 -0500

    POO: Constructores

--- java/programacion_estructurada/src/main/java/acc/coding/poo/Persona.java
+++ java/programacion_estructurada/src/main/java/acc/coding/poo/Persona.java
@@ -6,6 +6,7 @@ public class Persona {
     int edad;
     String apellido;
     double peso;
+    double masaCorporal;
 
     // Constructor: un metodo especial que me permite instancia un objeto de la
     // clase
@@ -16,14 +17,20 @@ public class Persona {
     // objeto.
     // Una clase puede tener mas de 1 constructor, pero no 2 constructores iguales
     public Persona() {
-        System.out.println("Creando una persona.");
+        System.out.println("Creando una persona. Constructor en blanco.");
     }
 
     public Persona(String name, String apellido, int edad, double peso) {
+        System.out.println("Creando una persona. Constructor con argumentos.");
         this.nombre = name;
         this.edad = edad;
         this.apellido = apellido;
         this.peso = peso;
+        this.masaCorporal = (peso * edad) / 0.24;
+    }
+
+    public Persona(String apellido) {
+        this.apellido = apellido;
     }
 
     // Metodos: funciones declaras a nivel de la clase (no seran static)
@@ -36,7 +43,15 @@ public class Persona {
     }
 
     public void dormir() {
+        this.trabajar();
         System.out.println(this.nombre + " esta durmiendo.");
+    }
+
+    // @ -> Anotacion: una caracteristica extra que se le anade una function o atributo
+    // Override: sobrecargando un metodo (sobrecargar = sobreescribiendo)
+    @Override
+    public String toString() {
+        return "Persona [nombre=" + nombre + ", edad=" + edad + ", apellido=" + apellido + "]";
     }
 
 }
--- java/programacion_estructurada/src/main/java/acc/coding/poo/Poo.java
+++ java/programacion_estructurada/src/main/java/acc/coding/poo/Poo.java
@@ -3,47 +3,68 @@ package acc.coding.poo;
 import java.util.Scanner;
 
 public class Poo {
-    /*
-     * Programacion Orientada a Objetos
-     * Es nuevo paradigma busca abstraer conceptos del mundo real dentro del codigo
-     * Cada representacion de un item del mundo real se denomina objeto.
-     * Dado que un objeto del mundo real es demasiado complejo en la programacion se utilizar el proceso de 'abstraccion' para obtener/asignar solo las caracteristicas deseadas en el programa.
-     * 
-     * Objeto se compone de 2 partes importantes:
-     * - atributos: caracteristicas que definen al objeto.
-     *      Auto: color, marca, peso, modelo, anio
-     * - metodos (funciones): acciones que un objeto puede realizar
-     *      Auto: prender, arrancar, cambiar_marcha, apagarse
-     * 
-     * En Orientacion a objetos veremos dos conceptos:
-     * - Clase: molde o plantilla para construir objetos
-     * - Objeto: una instancia de la clase.
-     */
+   /*
+    * Programacion Orientada a Objetos
+    * Es nuevo paradigma busca abstraer conceptos del mundo real dentro del codigo
+    * Cada representacion de un item del mundo real se denomina objeto.
+    * Dado que un objeto del mundo real es demasiado complejo en la programacion se
+    * utilizar el proceso de 'abstraccion' para obtener/asignar solo las
+    * caracteristicas deseadas en el programa.
+    * 
+    * Objeto se compone de 2 partes importantes:
+    * - atributos: caracteristicas que definen al objeto.
+    * Auto: color, marca, peso, modelo, anio
+    * - metodos (funciones): acciones que un objeto puede realizar
+    * Auto: prender, arrancar, cambiar_marcha, apagarse
+    * 
+    * En Orientacion a objetos veremos dos conceptos:
+    * - Clase: molde o plantilla para construir objetos
+    * - Objeto: una instancia de la clase.
+    */
 
+   public static void ejecutar() {
+      String str1 = "";
+      Scanner sc = new Scanner(System.in);
+      sc.close();
+      // Al declarar objetos siempre utilizaremos la palabra `new` para su
+      // instanciacion.
+      Persona p1 = new Persona();
+      // p1.nombre = "Alex";
+      p1.edad = 28;
+      p1.apellido = "Charco";
+      p1.peso = 100.25;
 
-     public static void ejecutar(){
-        /* String str1 = "";
-        Scanner sc = new Scanner(System.in); */
-        // Al declarar objetos siempre utilizaremos la palabra `new` para su instanciacion.
-        Persona p1 = new Persona();
-        p1.nombre = "Alex";
-        p1.edad = 28;
-        p1.apellido = "Charco";
-        p1.peso = 100.25;
+      Persona p2 = new Persona();
+      p2.nombre = "Jose";
+      p2.edad = 24;
+      p2.apellido = "Charco";
+      p2.peso = 10.25;
 
-        Persona p2 = new Persona();
-        p2.nombre = "Jose";
-        p2.edad = 24;
-        p2.apellido = "Charco";
-        p2.peso = 10.25;
+      System.out.println("Person1. Nombre= " + p1.nombre);
+      System.out.println("Person2. Nombre= " + p2.nombre);
 
-        System.out.println("Person1. Nombre= "+p1.nombre);
-        System.out.println("Person2. Nombre= "+p2.nombre);
+      System.out.println("p1 = " + p1);
+      System.out.println("p2 = " + p2);
 
-        System.out.println("p1 = "+p1);
-        System.out.println("p2 = "+p2);
-        
-        p1.trabajar();
-        p2.trabajar();
-     }
+      p1.trabajar();
+      p2.trabajar();
+
+      System.out.println("\n\n\n\n\n");
+      Persona p3 = new Persona("Cosme", "Fulanito", 42, 150.6);
+      System.out.println("Person3. Nombre= " + p3.nombre);
+      System.out.println("Person3. masaCorporal= " + p3.masaCorporal);
+      p3.nombre = "Homero";
+      System.out.println("Person3. Nombre= " + p3.nombre);
+      System.out.println(p3);
+      // Esto crea un objeto totalmente nuevo
+      p3 = new Persona();
+      System.out.println(p3);
+
+      Persona p4 = new Persona("Simpson");
+      p4.trabajar();
+      
+      System.out.println("\n\n\n\n\ntoString()");
+      System.out.println("p3.1 = "+p3.toString());
+      System.out.println("p3.2 = "+p3);
+   }
 }
